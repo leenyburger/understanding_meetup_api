5b6d2562643d9fcbb9121cec2346718c523f7e41
Colleen <colleen.schnettler@gmail.com>
2018-01-07 16:44:08 -0500

    Finished Understanding Meetup API - add personal API key in production'

--- app/controllers/meetup_controller.rb
+++ app/controllers/meetup_controller.rb
@@ -1,6 +1,8 @@
 class MeetupController < ApplicationController
   def index
     @events = Meetup.new.events
-    render json: @events
+    render json: @events, status: :ok
+  rescue StandardError => e
+    render json: { errors: e.message }, status: :unprocessable_entity
   end
 end
--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,12 +2,29 @@ class Meetup
   include HTTParty
   base_uri 'api.meetup.com'
 
+  attr_reader :options
+
+  def initialize
+    api_key  = YOUR_API_KEY
+    @options = {
+      query: {
+        key: api_key,
+        sign: "true",
+        desc: "true",
+        page: 20
+      }
+    }
+  end
+
   def get_data
-    self.class.get('/operation-code-hampton-roads/events')
+    self.class.get('/operation-code-hampton-roads/events', @options)
   end
 
   def events
-    get_data.parsed_response
+    if get_data.code.to_i == 200
+      get_data.parsed_response
+    else
+      raise "Error fetching data from Meetup API"
+    end
   end
 end
-
